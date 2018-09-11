bb579492f5957a926d541d2b35bf264ab2e542d6
nilden <stefan.nilden@gmail.com>
2018-09-11 14:05:56 +0200

    Lagt till tester för verify

--- lab3/src/main/java/se/cag/labs/CustomerService.java
+++ lab3/src/main/java/se/cag/labs/CustomerService.java
@@ -1,4 +1,26 @@
 package se.cag.labs;
 
+import javax.inject.Inject;
+
 public class CustomerService {
+
+    @Inject
+    private CustomerDao customerDao;
+
+    public boolean addCustomer(Customer customer){
+
+        if(customerDao.exists(customer.getCustomerId())){
+            return false;
+        }
+
+        return customerDao.save(customer);
+    }
+
+    public CustomerDao getCustomerDao() {
+        return customerDao;
+    }
+
+    public void setCustomerDao(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
 }
---
+++ lab3/src/test/java/se/cag/labs/CustomerServiceTest.java
@@ -0,0 +1,67 @@
+package se.cag.labs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+public class CustomerServiceTest {
+
+    @Mock
+    private CustomerDao daoMock;
+
+    @InjectMocks
+    private CustomerService service;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testAddCustomer_shouldReturnFalseIfCustomerExists() {
+
+        when(daoMock.exists(anyString())).thenReturn(true);
+
+        boolean result = service.addCustomer(new Customer("Ross", "1122333"));
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testAddCustomer_shouldReturnTrueIfCustomerDoesntExist() {
+
+        when(daoMock.exists(anyString())).thenReturn(false);
+        when(daoMock.save(any(Customer.class))).thenReturn(true);
+
+        boolean result = service.addCustomer(new Customer("Ross", "1122333"));
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void verifyThatMethodIsCalled() {
+
+        when(daoMock.save(any(Customer.class))).thenReturn(true);
+
+        Customer customer = new Customer("Ross", "1122333");
+        assertTrue(service.addCustomer(customer));
+
+        //verify that the save method has been invoked
+        verify(daoMock).save(any(Customer.class));
+        //the above is similar to :  verify(daoMock, times(1)).save(any(Customer.class));
+
+        //verify that the exists method is invoked one time
+        verify(daoMock, times(1)).exists(anyString());
+
+        //verify that the delete method has never been  invoked
+        verify(daoMock, never()).delete(any(Customer.class));
+    }
+}
